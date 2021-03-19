bd34e42f79258ec0dba2ea17590d6a69845d2915
IT PHP Team <svinaysyadav@gmail.com>
2021-03-19 17:47:58 +0530

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Todo Laravel')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12 col-sm-6 offset-sm-3 col-md-6 offset-md-3">
+      
+      <h1>Home page</h1>
+
+    </div>
+  </div>
+</div>
+@endsection
